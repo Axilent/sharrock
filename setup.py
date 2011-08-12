8320d66c76bc54711cf5807ff2b11aa16404f059
Loren Davie <loren.davie@gmail.com>
2011-08-12 15:50:31 -0400

    Re-organizing project to facilitate packaging.

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
 	version='0.1dev',
 	packages=['saaspire.sharrock','saaspire.sharrock_example'],
 	license='BSD',
-	long_description=open('../README.md').read(),
+	long_description=open('README.md').read(),
 )

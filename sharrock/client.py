83ae7fcf766f67f8fd05e8a2632f5d5a29eec46a
Loren Davie <loren@axilent.com>
2013-04-14 13:39:04 -0400

    Removing ugly debug

--- sharrock/client.py
+++ sharrock/client.py
@@ -336,13 +336,7 @@ class ResourceClient(object):
         Locally caches the resource descriptor.
         """
         if not self._descriptor or force:
-            print 'Asking for descriptor at','%s/describe/%s/%s/%s.json' % (self._service_url,self._app,self._version,self._resource_slug)
-            
             response = requests.get('%s/describe/%s/%s/%s.json' % (self._service_url,self._app,self._version,self._resource_slug))
-            
-            print 'Received raw response: %s' % response.text
-            
-            
             self._descriptor = response.json(strict=False)
 
             if 'get' in self._descriptor:

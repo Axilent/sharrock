bd7f017f9aecd185d1fc4869c78994bfb6186b56
Loren Davie <code@axilent.com>
2015-05-16 17:18:37 -0400

    Using new Django import for urls.

--- sharrock/urls.py
+++ sharrock/urls.py
@@ -1,15 +1,19 @@
 """
 URLs to mount Sharrock functions.
 """
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls import include, url, patterns
+except ImportError:
+    # old import pattern
+    from django.conf.urls.defaults import *
 
 urlpatterns = patterns('sharrock.views',
-    (r'^dir\.(?P<extension>\w+)$','directory'),
-    (r'^dir/$','directory',{'extension':'html'}),
-    (r'^dir/(?P<app>[\w\.-]+)/(?P<version>[\w\.-]+)/$','directory',{'extension':'html'}),
-    (r'^dir/(?P<app>[\w\.-]+)/(?P<version>[\w\.-]+)\.(?P<extension>\w+)$','directory'),
-    (r'^describe/(?P<app>[\w\.-]+)/(?P<version>[\w\.-]+)/(?P<service_name>[\w-]+)\.(?P<extension>\w+)$','describe_service'),
-    (r'^describe/(?P<app>[\w\.-]+)/(?P<version>[\w\.-]+)/(?P<service_name>[\w-]+)/$','describe_service',{'extension':'html'}),
-    (r'^(?P<app>[\w\.-]+)/(?P<version>[\w\.-]+)/(?P<service_name>[\w-]+)/$','execute_service',{'extension':'json'}),
-    (r'^(?P<app>[\w\.-]+)/(?P<version>[\w\.-]+)/(?P<service_name>[\w-]+)\.(?P<extension>\w+)$','execute_service'),
+    url(r'^dir\.(?P<extension>\w+)$','directory'),
+    url(r'^dir/$','directory',{'extension':'html'}),
+    url(r'^dir/(?P<app>[\w\.-]+)/(?P<version>[\w\.-]+)/$','directory',{'extension':'html'}),
+    url(r'^dir/(?P<app>[\w\.-]+)/(?P<version>[\w\.-]+)\.(?P<extension>\w+)$','directory'),
+    url(r'^describe/(?P<app>[\w\.-]+)/(?P<version>[\w\.-]+)/(?P<service_name>[\w-]+)\.(?P<extension>\w+)$','describe_service'),
+    url(r'^describe/(?P<app>[\w\.-]+)/(?P<version>[\w\.-]+)/(?P<service_name>[\w-]+)/$','describe_service',{'extension':'html'}),
+    url(r'^(?P<app>[\w\.-]+)/(?P<version>[\w\.-]+)/(?P<service_name>[\w-]+)/$','execute_service',{'extension':'json'}),
+    url(r'^(?P<app>[\w\.-]+)/(?P<version>[\w\.-]+)/(?P<service_name>[\w-]+)\.(?P<extension>\w+)$','execute_service'),
 )
